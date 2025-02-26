aed7f563c8bdd5fa54364a43b17d8284cb7d63f2
Aleem Khusro <aleem.khusro@gmail.com>
2025-02-26 18:54:16 +0500

    Added the single attention head of a transformer

--- gpt.py
+++ gpt.py
@@ -5,13 +5,12 @@ from torch.nn import functional as F
 #hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3 #decrease the learning because self attention cant tolerate very high learning rate
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
-
 #-------
 
 torch.manual_seed(1337)
@@ -65,16 +64,44 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    """
+    one head of self attention
+    """
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size,block_size)))
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x) #shape of B,T,head_size # head_size is also referred to as the channel dimension in the comments below
+        q = self.query(x) #shape of B,T,head_size
+        v = self.value(x) #shape of B,T,head_size
+
+        #compute attention scores
+        wei = q @ k.transpose(-1,-2) * C**-0.5 # (B,T,C) @ (B,C,T) -> (B,T,T) these will be the attention affinities
+        wei = wei.masked_fill(self.tril[:T, :T]==0, float('-inf'))
+        wei = F.softmax(wei, dim=-1) #B,T,T #the softmax will give the attention score
+        # perform weighted aggregation
+        out = wei @ v #wei: B,T,T and v: B,T,head_size -> B,T,head_size
+        return out
+
 
 class BigramLanguageModel(nn.Module):
-
+    #we are going to modify this to incldue the attention head
     def __init__(self):
         #no need to pass the vocab size arouond as it is already defined uptop
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         #positional emebdinng Notice how the second dimension is the same as the token embedding dimension
+        #for the positional encoding we just index it with arange tensor, and uptill the context we have and less than block size always during forward and inference.
+        #see below for more details on positional embedding.
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd) #for now the head_size will be just as n_embd
 
         #this is now new linear layer that will apply soon after the token emmbeddings.
         self.lm_head = nn.Linear(n_embd, vocab_size)
@@ -87,9 +114,11 @@ class BigramLanguageModel(nn.Module):
         #instead these will just be the token embeddings.
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         #this will right now just create the same block_size by n_embd tensor. It just indexes the position embedding table with 0-7 indices, and it will just
-        #pluck out the entire position embedding table
+        #pluck out the entire position embedding table, or uptill T if we are doing inference with incremental context size.
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
-        x = tok_emb + pos_emb
+        x = tok_emb + pos_emb #B,T,C
+        x = self.sa_head(x) #apply one head of self attention B,T,C
+
         #the below linear layer is basically a matrix multiplaction
         #the (4,8,32) @ (32,65) produces a tensor of (4,8,65)
         logits = self.lm_head(x) # (B,T,Vocab_Size)
@@ -107,8 +136,15 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            #one more thing we need to add because we have added positional encoding is to crop the context other wise
+            # the positional encoding table will run out of scope, as the positional encoding is defined as
+            #self.position_embedding_table = nn.Embedding(block_size, n_embd) so we need to crop the input
+            # doing -block_size as the indexer for the column dimension is okay even if the idx tensor is say of size (8,3) and our block size is of 9 for eg, and if index it like this
+            #idx[:,-9:] this means that i want all the rows and i want all the columns starting from 9 places to the end of the tensor. But it will still work if we only have 3 columns so far, and
+            # we dont have to worry about going out of bounds with negative indexing, it will just pick out the elements that are available.
+            idx_cond = idx[:, -block_size:] #reassign to new variable so that the current generation doesn't get overwritten.
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
@@ -135,7 +171,9 @@ for iter in range(max_iters):
     optimizer.step()
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens = 500)[0].tolist()))
+generated = m.generate(context, max_new_tokens = 500)[0].tolist()
+print(len(generated))
+print(decode(generated))
 
 
 
